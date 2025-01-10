28e00910a8b84ebf6eed0b3dec95a1fbf00afb11
Weberson Linux <webersonmorelli2004@gmail.com>
2025-01-10 16:49:45 -0300

    deletando o code1 antigo

--- code1.py
+++
@@ -1,9 +0,0 @@
-def ditar_alfabeto(num, alf):
-    for i in range(num):
-        print(alf[i])
-
-alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-
-ditar_alfabeto(26, alfabeto)
-
